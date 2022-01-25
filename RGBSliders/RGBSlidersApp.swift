4d54bc52d9deb725658d378022c60a98aad10fc9
AlexeyBH <alexey_bh@mail.ru>
2022-01-25 18:44:48 +0300

    Initial Commit

---
+++ RGBSliders/RGBSlidersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBSlidersApp.swift
+//  RGBSliders
+//
+//  Created by Alexey Khestanov on 25.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct RGBSlidersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
