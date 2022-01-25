6d715cec3a849963467bf591cf3b8c19f6b63bc2
AlexeyBH <alexey_bh@mail.ru>
2022-01-25 23:04:11 +0300

    Initial commit

--- RGBSliders/ContentView.swift
+++ RGBSliders/ContentView.swift
@@ -8,10 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var red: (val: Double, str: String) = (128.0, "128")
+    @State var green: (val: Double, str: String) = (128.0, "128")
+    @State var blue: (val: Double, str: String) = (128.0, "128")
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundColor(Color.init(
+                    .sRGB,
+                    red: red.val / 255,
+                    green: green.val / 255,
+                    blue: blue.val / 255
+                ))
+            Spacer()
+            SliderGroup(
+                color: .red,
+                range: 0...255,
+                str: $red.str,
+                value: $red.val
+            )
+            SliderGroup(
+                color: .green,
+                range: 0...255,
+                str: $green.str,
+                value: $green.val
+            )
+            SliderGroup(
+                color: .blue,
+                range: 0...255,
+                str: $blue.str,
+                value: $blue.val
+            )
+        }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ RGBSliders/SliderGroup.swift
@@ -0,0 +1,103 @@
+//
+//  SliderGroup.swift
+//  RGBSliders
+//
+//  Created by Alexey Khestanov on 25.01.2022.
+//
+
+import SwiftUI
+
+struct SliderGroup: View {
+
+    let color: Color
+    let range: ClosedRange<Double>
+
+    @Binding var str: String
+    @Binding var value: Double
+    
+    @State var isAlertPresented = false
+    @FocusState private var isFocused: Bool
+
+    private let scrWidth = UIScreen.main.bounds.width
+    
+    var body: some View {
+        HStack {
+            slider
+                .accentColor(color)
+                .frame(
+                    width: scrWidth * 0.75,
+                    height: scrWidth / 30,
+                    alignment: .leading
+                )
+            Spacer()
+            textField
+                .alert("Bad input", isPresented: $isAlertPresented)
+            {
+                Button("Ok") {
+                    changedSlider(false)
+                }
+            }
+        }
+        .padding()
+    }
+    
+    private var slider: some View {
+        Slider(
+            value: $value,
+            in: range,
+            step: 1,
+            onEditingChanged: changedSlider
+        )
+    }
+    
+    private func changedText() -> Void {
+        guard let newVal = Double(str),
+              newVal >= range.lowerBound,
+              newVal > range.upperBound
+        else {
+            isAlertPresented = true
+            return
+        }
+        if newVal != value {
+            value = newVal
+        }
+        isFocused = false
+    }
+    
+    private var textField: some View {
+        TextField("", text: $str)
+            .foregroundColor(color)
+            .focused($isFocused)
+            .onSubmit { changedText() }
+            .keyboardType(.numberPad)
+            .toolbar {
+                ToolbarItem(placement: .keyboard) {
+                    HStack {
+                        Button("Done") { changedText() }
+                    }
+                }
+            }
+    }
+    
+    private func changedSlider(_: Bool) -> Void {
+        let newStr = String(Int(value))
+        if newStr != str {
+            str = newStr
+        }
+    }
+    
+}
+
+struct SliderGroup_Previews: PreviewProvider {
+    static var previews: some View {
+        SliderGroup(
+            color: .red,
+            range: 0.0...255.0,
+            str: .constant("100"),
+            value: .constant(100)
+        )
+    }
+}
+
+
+///
